cedaecc6f357a6d078e5b101f69185c8e07ee9de
ryuta <ryuta0221@gmail.com>
2015-01-12 20:23:07 +0900

    applicationController一部修正

--- app/services/BlogDataController.java
+++ app/services/BlogDataController.java
@@ -1,7 +1,13 @@
 package services;
 
+import com.avaje.ebean.Ebean;
 import com.fasterxml.jackson.databind.JsonNode;
+import models.BlogData;
 import org.springframework.stereotype.Service;
+import play.libs.Json;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by ryuta on 15/01/12.
@@ -10,7 +16,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogDataController {
 
-    public void findBlogs(JsonNode request) {
+    public void findBlogs(String json) {
+        JsonNode node = Json.parse(json);
 
+        String title = node.get("title").asText();
+        String state = node.get("status").asText();
+        String publishFrom = node.get("publishFrom").asText();
+        String publishTo = node.get("publishTo").asText();
     }
 }
