1b8934b243969a191fe5b10602408f766779853b
ryuta <ryuta0221@gmail.com>
2015-01-12 17:32:19 +0900

    Springを使ったDI,コントローラクラスインスタンス化テスト

--- app/Global.java
+++ app/Global.java
@@ -6,18 +6,36 @@ import play.GlobalSettings;
 import play.Logger;
 
 /**
+ * アプリケーション起動時の初期設定などを行う
+ * クラス名をGlobalにすることで、PlayFrameworkから自動的に呼ばれる
+ * ようになる。
+ *
  * Created by ryuta on 15/01/12.
  */
 public class Global extends GlobalSettings {
 
     private  ApplicationContext context;
 
+    /**
+     * アプリケーション起動時、最初に呼び出される。
+     * SpringのApplicationContextインスタンスを生成。
+     * @param app
+     */
     @Override
     public void onStart(Application app) {
         System.out.println("on Start");
         context = new ClassPathXmlApplicationContext("applicationContext.xml");
     }
 
+    /**
+     * ApplicationContextから取得したインスタンスを返す。
+     * routesがコントローラインスタンスを生成する際に呼び出される。
+     *
+     * @param controllerClass
+     * @param <A>
+     * @return
+     * @throws Exception
+     */
     @Override
     public <A> A getControllerInstance(Class<A> controllerClass) throws Exception {
         System.out.println("GETCONTROLLERINSTANCE");
